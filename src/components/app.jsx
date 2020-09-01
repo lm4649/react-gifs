ca6f5d23d83207c8d85c17770841fdca88625b88
marc lopes <marc_lopes2004@yahoo.fr>
2020-09-01 21:11:02 +0900

    implemented search giph function in App class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import SearchBar from './search-bar';
 import Gif from './gif';
 import GifList from './gif-list';
@@ -8,16 +9,29 @@ class App extends Component {
     super(props);
 
     this.state = {
-      selectedGifId : "pOZhmE42D1WrCWATLK"
-    }
+      selectedGifId : "pOZhmE42D1WrCWATLK",
+      gifs: []
+    };
+
+    this.search('disney');
+  }
+
+  search = (query) => {
+    giphy('MvxQUzAfmZvFd3ZiLX3kk7dOyd2Ocahn').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      this.setState({ gifs: res.data });
+    });
   }
 
 
   render() {
-    const gifs = [
-        { id: "pOZhmE42D1WrCWATLK" },
-        { id: "26AHONQ79FdWZhAI0" }
-      ];
+    // const gifs = [
+    //     { id: "pOZhmE42D1WrCWATLK" },
+    //     { id: "26AHONQ79FdWZhAI0" }
+    //   ];
 
     return (
       <div>
@@ -31,7 +45,7 @@ class App extends Component {
         </div>
         <div className='right-scene'>
           <div className="gif-list">
-            <GifList gifs={gifs} />
+            <GifList gifs={this.state.gifs} />
           </div>
         </div>
       </div>
